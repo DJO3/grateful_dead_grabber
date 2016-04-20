9f08f7d48bb3ae7cf90352ecf32b6524a77256cb
Dave O'Connor <daveoconnor3@gmail.com>
2016-04-19 22:30:08 -0400

    initial js implementation of highchairs column-basic

---
+++ static/js/column-basic.js
@@ -0,0 +1,80 @@
+init();
+
+function init() {
+    getSeries(['grateful-dead', 'allman-brothers', 'dave-matthews-band'], 'month', function(series) {
+        columnBasic(series);
+    });
+}
+
+// Asynchronously get monthly show stats. Accepts array of artists and selector string 'day', 'month', 'year'
+function getSeries (artists, selector, callback) {
+    series = [];
+    for (var i = 0; i < artists.length; i++) {
+        var url = '/api/v1/shows/' + artists[i] + '?count=' + selector;
+        var ajaxData = $.ajax({
+            url: url,
+            indexValue: i,
+            success: function (result) {
+                var name = artists[this.indexValue];
+                var data = result.data.count;
+                series.push({"name": name, "data": data});
+
+                if (series.length === artists.length) {
+                    callback(series)
+                }
+            }
+        });
+    }
+}
+
+function columnBasic(series) {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Shows Performed'
+        },
+        subtitle: {
+            text: 'Source: Setlist.fm'
+        },
+        xAxis: {
+            categories: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of shows'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y} shows</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: series
+    });
+}
