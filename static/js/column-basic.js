c8351e0e8c19504dab0915c3caeeb0e920294348
Dave O'Connor <daveoconnor3@gmail.com>
2016-04-30 17:47:29 -0400

    added basic-line placeholder

--- static/js/column-basic.js
+++ static/js/column-basic.js
@@ -28,7 +28,7 @@ $('#column-basic').on("click", function() {
 });
 
 // Load pie-drildown chart on click, wiggle briefly on mouseenter
-$('#pie_drilldown').on("click", function() {
+$('#pie-drilldown').on("click", function() {
     $(this).ClassyWiggle('stop');
     // var defaultSelector = 'month';
     // var artists = getCheckedArtists();
@@ -40,6 +40,25 @@ $('#pie_drilldown').on("click", function() {
     //     columnBasic(categories, series);
     // });
     pieDrillDown();
+}).on("mouseenter", function() {
+    $(this).ClassyWiggle('start', {limit: 2});
+}).on("mouseleave", function() {
+    $(this).ClassyWiggle('stop');
+});
+
+// Load pie-drildown chart on click, wiggle briefly on mouseenter
+$('#basic-line').on("click", function() {
+    $(this).ClassyWiggle('stop');
+    // var defaultSelector = 'month';
+    // var artists = getCheckedArtists();
+    // console.log(artists);
+    $('#ordinal-selector').hide();
+    $('#modal-id').modal('show');
+    //
+    // getSeries(artists, defaultSelector, function(categories, series) {
+    //     columnBasic(categories, series);
+    // });
+    basicLine();
 }).on("mouseenter", function() {
     $(this).ClassyWiggle('start', {limit: 2});
 }).on("mouseleave", function() {
@@ -313,6 +332,56 @@ function pieDrillDown() {
                 ]
             }]
         }
+    });
+}
+
+// Loads basicLine chart
+function basicLine() {
+    $('#container').highcharts({
+        title: {
+            text: 'Average Number of Songs Played Per Show',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: Setlist.fm',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
     });
 }
 
